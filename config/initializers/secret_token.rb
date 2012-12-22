2b0b22db41a3a500f685890abd179611edf55c85
Weston Platter <westonplatter@gmail.com>
2012-12-22 15:24:52 -0600

    the beginning of awesome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upvote::Application.config.secret_token = 'd286f327f170eab322a091cb1ca65108c51a1bd2960d958d2b32427e027a35e8a1816afe369f953987a621dd19541724674d58560050a704ba174d2dab8268bf'
