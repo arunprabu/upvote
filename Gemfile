76f23110fbc36361bc03cd1962bfec8234554149
Weston Platter <westonplatter@gmail.com>
2012-12-22 16:21:38 -0600

    added devise i could create account

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.9'
 gem 'sqlite3'
+gem 'devise'
 
 group :test, :development do
   gem 'rspec-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Upvote::Application.routes.draw do
   get 'dashboard/index'
   get 'dashboard' => 'dashboard#index'
 
+  devise_for :users
+
   root :to => 'dashboard#index'
 end
